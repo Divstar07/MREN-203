777ca9ec005c39473bed0e3c1afbbb1ee2fa3788
ThreeDucks22 <22xrt@queensu.ca>
2025-02-03 10:12:59 -0500

    Update SharpSensors.ino

--- SharpSensors.ino
+++ SharpSensors.ino
@@ -5,6 +5,12 @@ const byte SHARP_PIN = A5;
 int sharp_val = 0; // integer read from analog pin
 float sharp_range; // range measurement [cm]
 
+float convertToCm(int num)
+{
+  float dist = 1578.9*pow(num, 0.82);
+  return dist;
+}
+
 void setup()
 {
 // Open the serial port at 115200 bps
@@ -15,9 +21,10 @@ void loop()
 {
   // Read the sensor output (0-1023, which is 10 bits and fits inside an Arduino int-type),→
   sharp_val = analogRead(SHARP_PIN);
+  sharp_range = convertToCm(sharp_val);
 
   // Print all values
-  Serial.print(sharp_val);
+  Serial.print(sharp_range);
   Serial.print("\n");
 
   // Delay for a bit before reading the sensor again
