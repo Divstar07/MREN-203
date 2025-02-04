5e44dc8851999e92ffead1cfceef2e1e1b666fd4
ThreeDucks22 <22xrt@queensu.ca>
2025-02-04 09:47:26 -0500

    Update SharpSensors.ino
    added left and right sensors, adjusted conversion to cm

--- SharpSensors.ino
+++ SharpSensors.ino
@@ -1,13 +1,21 @@
 // Arduino analog input pin to which the Sharp sensor is connected
-const byte SHARP_PIN = A5;
+const byte SHARP_PIN_FRONT = A5;
+const byte SHARP_PIN_LEFT = A4;
+const byte SHARP_PIN_RIGHT = A3;
 
 // Variables to store the proximity measurement
-int sharp_val = 0; // integer read from analog pin
-float sharp_range; // range measurement [cm]
+int sharp_val_front = 0; // integer read from analog pin
+float sharp_range_front; // range measurement [cm]
+
+int sharp_val_left = 0;
+float sharp_range_left;
+
+int sharp_val_right = 0;
+float sharp_range_right;
 
 float convertToCm(int num)
 {
-  float dist = 1578.9*pow(num, 0.82);
+  float dist = 1578.9*pow(num, 0.82) + 5;
   return dist;
 }
 
@@ -20,11 +28,21 @@ void setup()
 void loop()
 {
   // Read the sensor output (0-1023, which is 10 bits and fits inside an Arduino int-type),→
-  sharp_val = analogRead(SHARP_PIN);
-  sharp_range = convertToCm(sharp_val);
+  sharp_val_front = analogRead(SHARP_PIN_FRONT);
+  sharp_range_front = convertToCm(sharp_val_front);
+
+  sharp_val_left = analogRead(SHARP_PIN_LEFT);
+  sharp_range_left = convertToCm(sharp_val_left);
+
+  sharp_val_right = analogRead(SHARP_PIN_RIGHT);
+  sharp_range_right = convertToCm(sharp_val_right);
 
   // Print all values
-  Serial.print(sharp_range);
+  Serial.print(sharp_range_left);
+  Serial.print("\t |");
+  Serial.print(sharp_range_front);
+  Serial.print("\t |");
+  Serial.print(sharp_range_right);
   Serial.print("\n");
 
   // Delay for a bit before reading the sensor again
